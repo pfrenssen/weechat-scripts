899c613c236cf6c53630bf67b756378629499f9f
shmibs <shmibs@gmail.com>
2013-04-17 22:26:31 -0700

    clean things up and expand word list

--- wat.pl
+++ wat.pl
@@ -3,7 +3,7 @@ use warnings;
 use utf8;
 use List::Util 'shuffle';
 
-weechat::register ("wat", "shmibs", "0.3", "GPL", "replace words at random ( usage: enable/disable with /wat <on|off> )", "", "");
+weechat::register ("wat", "shmibs", "0.3", "GPL", "intentionally obfuscate speach ( usage: enable/disable with /wat <on|off> )", "", "");
 weechat::hook_command("wat", "", "", "", "", "toggle", "");
 weechat::hook_modifier("input_text_content", "catch_word", "");
 weechat::hook_modifier("input_text_for_buffer", "catch_send", "");
@@ -15,7 +15,17 @@ my @replacements = (
 	'foreign', 'dumpling', 'phalanges', 'goose', 'didactic',
 	'vroom', 'zygote', 'tractor', 'blatant', 'authoritatively',
 	'feral', 'dung', 'scones', 'busted', 'tortoise',
-	'willingly', 'excited', 'glorious'
+	'willingly', 'excited', 'glorious', 'torpor', 'light',
+	'gracious', 'cretan', 'variantly', 'stammers', 'knifed',
+	'slashed', 'occupational', 'dissipated', 'Singapore',
+	'afterglow', 'toes', 'whorls', 'sneakiness', 'dodgers',
+	'Johannesburg', 'inextricable', 'slaughterhouse',
+	'prophecy', 'lobster', 'contraption', 'hook', 'zoos',
+	'beafy', 'impersonated', 'telescope', 'countess',
+	'marsupial', 'boots', 'guided', 'titillating', 'bonds',
+	'bequeathing', 'delineation', 'associators', 'ballpark',
+	'stultify', 'ionise', 'expanders', 'cybernetics',
+	'alluring', 'medicinal', 'bloom'
 );
 
 my $state="off";
@@ -77,32 +87,40 @@ sub mutate {
 	my $punctuation=0;
 
 	if($words[$#words]) {
-		if( $words[$#words]=~/.|,|'|"|\?|!|:|;|[|]|(|)/ ) {
+
+		# maintain the position of punctuation
+		if( $words[$#words]=~/[.,'"\?!:;\[\]\(\)]/ ) {
 			$punctuation=substr($words[$#words], length($words[$#words])-1, 1);
 			$words[$#words]=substr($words[$#words], 0, length($words[$#words])-1);
 		}
 		
-		# apply desired transformations to the most
-		# recently typed word here
+		# grab a random replacement word from the list
 		if( !int(rand(20)) ) {
 			$words[$#words]=$replacements[int(rand(@replacements))];
 		}
-	}
 	
-	# shuffle all words in sentence
-	if( !int(rand(20)) ) {
-		@words=shuffle(@words);
-	}
-	
-	# shuffle a single word
-	if( !int(rand(15)) ) {
-		my @chars=split(//, $words[$#words]);
-		@chars=shuffle(@chars);
-		$words[$#words]=join('', @chars);
-	}
-	
-	if($punctuation) {
-		$words[$#words].=$punctuation;
+		# swap two words at random
+		if( !int(rand(15)) ) {
+			my $i=int(rand(@words));
+			my $j=int(rand(@words));
+	        @words[$i,$j]=@words[$j,$i];
+		}
+		
+		# induce typos
+		if( !int(rand(10)) ) {
+			my @chars=split(//, $words[$#words]);
+			my $i=int(rand(@chars));
+			my $j=int(rand(@chars));
+	        @chars[$i,$j]=@chars[$j,$i];
+			$words[$#words]=join('', @chars);
+		}
+		
+		# maintain the position of punctuation, part
+		# two
+		if($punctuation) {
+			$words[$#words].=$punctuation;
+		}
+
 	}
 	
 	my $rval=join(' ', @words);
